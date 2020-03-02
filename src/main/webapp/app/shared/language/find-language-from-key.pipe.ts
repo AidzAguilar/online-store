9d950ec4ce206fa8791038f6350fb9ca3855f42d
Christian Adrian Aguilar <aaguilar@ninth-wave.com>
2020-03-02 19:56:36 +0800

    add Tamil as additional language

--- src/main/webapp/app/shared/language/find-language-from-key.pipe.ts
+++ src/main/webapp/app/shared/language/find-language-from-key.pipe.ts
@@ -4,7 +4,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FindLanguageFromKeyPipe implements PipeTransform {
   private languages: { [key: string]: { name: string; rtl?: boolean } } = {
     'zh-tw': { name: '繁體中文' },
-    en: { name: 'English' }
+    en: { name: 'English' },
+    ta: { name: 'தமிழ்' }
     // jhipster-needle-i18n-language-key-pipe - JHipster will add/remove languages in this object
   };
 
